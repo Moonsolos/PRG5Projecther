ccf3fe78b95789108ab7f5eb7fef13b729da8e49
Moonsolos <1056860@hr.nl>
2024-01-15 15:20:09 +0100

    linked database, created login/register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AboutUsController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
+Route::get('/', function () {
     return view('welcome');
 });
-//about us route
-Route::get('/about-us', function() {
-    return view('about-us');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/test', [AboutUsController::class, 'index']);
-
-Route::resource('products', ProductController::class);
-
-Route::get('about-us/{name}', [AboutUsController::class, 'showName']);
+require __DIR__.'/auth.php';
